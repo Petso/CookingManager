7d5aeafe989cd158953faf4fe701e743a1440c77
James Teng <James Teng>
2016-07-26 03:04:28 -0400

    fixed cursor offset + improved burger stacking

---
+++ CookingManagerV1/Assets/Scripts/Game Scripts/BurgerStacking.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurgerStacking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "burger") {
+			col.gameObject.transform.position = new Vector3 (gameObject.transform.position.x, 
+				gameObject.transform.position.y + gameObject.GetComponent<Collider>().bounds.size.y/2 + col.gameObject.GetComponent<Collider>().bounds.size.y/2, 
+				gameObject.transform.position.z);
+
+			//set parent to plate
+			if (gameObject.transform.parent == null) {
+				col.gameObject.transform.SetParent (gameObject.transform);
+			} else {
+				col.gameObject.transform.SetParent (gameObject.transform.parent.transform);
+			}
+
+			col.gameObject.AddComponent<BurgerStacking> ();
+			Destroy (col.gameObject.GetComponent<Rigidbody> ());
+
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- CookingManagerV1/Assets/Scripts/Game Scripts/mouseDrag.cs
+++ CookingManagerV1/Assets/Scripts/Game Scripts/mouseDrag.cs
@@ -3,12 +3,42 @@ using System.Collections;
 
 public class mouseDrag : MonoBehaviour {
 
-	float distance = 25;
+	float distance = 20;
 
-	// Use this for initialization
 	void OnMouseDrag () {
-		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-		Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		Camera cam = null;
+
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = distance;
+
+		//determines which camera to use
+		foreach (Camera c in Camera.allCameras) {
+			if (c.gameObject.name == "CameraFront") {
+				if (c.ScreenToWorldPoint (mousePosition).x < -23) {
+					foreach (Camera d in Camera.allCameras) {
+						if (d.gameObject.name == "CameraLeft") {
+							cam = d;
+							Debug.Log ("left");
+						}
+					}
+				} else if (c.ScreenToWorldPoint (mousePosition).x > 23) {
+					foreach (Camera d in Camera.allCameras) {
+						if (d.gameObject.name == "CameraRight") {
+							cam = d;
+							Debug.Log ("right");
+						}
+					}
+				} else {
+					cam = c;
+					Debug.Log ("front");
+				}
+		
+			}
+		}
+			
+		//set object position to mouse position based on chosen camera
+		mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = cam.ScreenToWorldPoint(mousePosition);
 
 		transform.position = objPosition;
 	}
